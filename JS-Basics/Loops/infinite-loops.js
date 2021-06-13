e4c619950108ebc370cae812ad750102f2c8aa23
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-13 20:52:01 +0300

    how to implement loops that are infinite in JavaScript

---
+++ JS-Basics/Loops/infinite-loops.js
@@ -0,0 +1,27 @@
+// while infinite loop
+while (true) {
+  // code to be executed
+}
+
+let i = 0;
+
+while (i < 5) {
+  console.log(i);
+  //   i++;
+}
+
+// ______________________________________________________________
+
+// do...while infinite loop
+let x = 0;
+
+do {
+  // x++;
+} while (x < 5);
+
+// ______________________________________________________________
+
+// for... infinite loop
+for (i = 0; i < 10; ) {
+  // code to be executed
+}
