692e1302c326aad6eb9ea9ec50091b8c8b676f85
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-13 22:35:57 +0300

    for...of loop in JavaScript

---
+++ JS-Basics/Loops/for...of-loop.js
@@ -0,0 +1,7 @@
+// difference between for...in loop and for...of loop
+// we use the for...in loop to iterate over the properties of an object and
+// for of loop to iterate over the elements or items in an array.
+
+const colors = ["red", "green", "blue"];
+
+for (let color of colors) console.log(color);
