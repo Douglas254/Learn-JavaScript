c45afaed66723821120b25ee6051fa471fced8ce
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-13 13:53:38 +0300

    initialize variable with let keyword

--- JS-Basics/Loops/for...loop.js
+++ JS-Basics/Loops/for...loop.js
@@ -3,18 +3,18 @@
 // }
 
 // program to display hello World! 5 times on the console
-for (i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
   console.log("Hello World!", i);
 }
 
 // Decrement above code in reverse order
-for (i = 5; i >= 1; i--) {
+for (let i = 5; i >= 1; i--) {
   console.log("Hello World", i);
 }
 
 // program to display odd numbers between 0 - 100
 
-for (i = 0; i <= 100; i++) {
+for (let i = 0; i <= 100; i++) {
   if (i % 2 !== 0) {
     console.log("Odd numbers are: ", i);
   }
