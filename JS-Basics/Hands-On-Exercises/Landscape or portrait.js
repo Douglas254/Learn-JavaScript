39e7d83b1eafbef8e1dbecb8f4fcd5687e1464c1
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-14 17:10:05 +0300

    function that return true if the width is greater than the height denoting landscape otherwise returns false showing it's a portrait picture.

---
+++ JS-Basics/Hands-On-Exercises/Landscape or portrait.js
@@ -0,0 +1,8 @@
+// if the dimensions entered for width are greater thanthose of height
+// the isLandscape function will return true it's a landscape otherwise portrait denoting false.
+
+function isLandscape(width, height) {
+  return width > height;
+}
+
+console.log(isLandscape(800, 600)); // calling isLandscape(); function
